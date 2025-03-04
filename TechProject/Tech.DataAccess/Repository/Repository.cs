e7036397788ce517dfd21d7209abe44ac2d81df9
rafat shepl <rafatshepl17@gmail.com>
2025-03-04 17:14:19 +0200

    add user carts

--- TechProject/Tech.DataAccess/Repository/Repository.cs
+++ TechProject/Tech.DataAccess/Repository/Repository.cs
@@ -46,5 +46,25 @@ namespace Tech.DataAccess.Repository
         {
             dbSet.RemoveRange(entites);
         }
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return query.ToList();
+        }
+
     }
 }
