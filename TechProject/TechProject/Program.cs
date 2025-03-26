fefe30b28b791db453aa6f9690dcecc99cf72c5f
rafat shepl <rafatshepl17@gmail.com>
2025-03-26 21:46:08 +0200

    add user page and regesteration fileds

--- TechProject/TechProject/Program.cs
+++ TechProject/TechProject/Program.cs
@@ -5,6 +5,7 @@ using TechProject.DataAccess.Data;
 using Microsoft.AspNetCore.Identity;
 using Tech.Utility;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using TechProject.Models;
 
 namespace TechProject
 {
@@ -56,9 +57,61 @@ namespace TechProject
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
-            
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                CreateAdminUser(services).Wait();
+            }
             app.Run();
         }
+        private static async Task CreateAdminUser(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                string adminRole = "Admin";
+                string adminEmail = "adminUser@gmail.com";
+                string adminPassword = "Admin@123456";
+
+               
+                if (!await context.Roles.AnyAsync(r => r.Name == adminRole))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(adminRole));
+                }
+
+                
+                if (!await context.Users.AnyAsync(u => u.Email == adminEmail))
+                {
+                    var hasher = new PasswordHasher<ApplicationUser>();
+
+                    var adminUser = new ApplicationUser
+                    {
+                        UserName = adminEmail,
+                        Email = adminEmail,
+                        EmailConfirmed = true,
+                        City = "AdminCity",
+                        BirthDate = new DateTime(1990, 1, 1), 
+                        PasswordHash = hasher.HashPassword(null, adminPassword)
+                    };
+
+                    context.Users.Add(adminUser);
+                    await context.SaveChangesAsync();
+
+                   
+                    var adminUserRole = new IdentityUserRole<string>
+                    {
+                        UserId = adminUser.Id,
+                        RoleId = context.Roles.First(r => r.Name == adminRole).Id
+                    };
+
+                    context.UserRoles.Add(adminUserRole);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+
     }
 
 }
